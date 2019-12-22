827883d166d78c854b622ed2734cfe3bd983ed90
TenDar <54836300+datnt908@users.noreply.github.com>
2019-12-22 16:26:08 +0700

    Add font-awesome icons as SVGR
    "yarn add react-native-svg"

---
+++ src/assets/icons/ForwardSolid.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Svg, { Path } from 'react-native-svg'
+
+const SvgComponent = props => (
+  <Svg
+    aria-hidden="true"
+    focusable="false"
+    data-prefix="fas"
+    data-icon="forward"
+    className="svg-inline--fa fa-forward fa-w-16"
+    role="img"
+    viewBox="0 0 512 512"
+    {...props}
+  >
+    <Path
+      fill={props.fill}
+      d="M500.5 231.4l-192-160C287.9 54.3 256 68.6 256 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2zm-256 0l-192-160C31.9 54.3 0 68.6 0 96v320c0 27.4 31.9 41.8 52.5 24.6l192-160c15.3-12.8 15.3-36.4 0-49.2z"
+    />
+  </Svg>
+)
+
+export default SvgComponent
---
+++ src/assets/icons/index.js
@@ -0,0 +1,23 @@
+import BackwardSolidSVGR from "./BackwardSolid";
+import BarSolidSVGR from "./BarSolid";
+import CaretDownSolidSVGR from "./CaretDownSolid";
+import ForwardSolidSVGR from "./ForwardSolid";
+import PauseSolidSVGR from "./PauseSolid";
+import PlaySolidSVGR from "./PlaySolid";
+import PlusSolidSVGR from "./PlusSolid";
+import RandomSolidSVGR from "./RandomSolid";
+import RepeatSolidSVGR from "./RepeatSolid";
+import TimesSolidSVGR from "./TimesSolid";
+
+export {
+  BackwardSolidSVGR,
+  BarSolidSVGR,
+  CaretDownSolidSVGR,
+  ForwardSolidSVGR,
+  PauseSolidSVGR,
+  PlaySolidSVGR,
+  PlusSolidSVGR,
+  RandomSolidSVGR,
+  RepeatSolidSVGR,
+  TimesSolidSVGR,
+}
