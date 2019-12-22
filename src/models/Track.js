c8a6974e68fd9e27ec8ce6e2f6abefb5f8149fdc
TenDar <54836300+datnt908@users.noreply.github.com>
2019-12-22 14:28:37 +0700

    Create Track model and helper-functions

---
+++ src/models/Track.js
@@ -0,0 +1,33 @@
+import { hashStringToInteger } from "../utils/helperFunctions";
+
+export default class Track {
+  constructor() {
+    this.id = null;
+    this.url = null;
+    this.title = null;
+    this.artist = null;
+    this.artwork = null;
+    this.duration = null;
+  }
+
+  loadDataFromFile = (file) => {
+    this.id = hashStringToInteger(file.filePath);
+    this.url = file.filePath;
+    this.title = file.fileName
+      .replace(/-/g, " ")
+      .replace(/_/g, " ")
+      .replace(".mp3", "");
+    this.artist = "Unknow artist";
+    this.artwork = "";
+    this.duration = 0;
+  }
+
+  loadDataFromSchema = (schema) => {
+    this.id = schema.id;
+    this.url = schema.url;
+    this.title = schema.title;
+    this.artist = schema.artist;
+    this.artwork = schema.artwork;
+    this.duration = schema.duration;
+  }
+}
